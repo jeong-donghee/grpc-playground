4d3d38585c8c1bc0fad2bf9ee4638a38bfdf1821
jeongdonghee <dh.jeong@ex-em.com>
2025-05-09 14:06:14 +0900

    init project

---
+++ src/main/java/com/github/grpc_playground/GrpcPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.github.grpc_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/grpc_playground/GrpcPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.grpc_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
